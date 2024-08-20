fcb28a214e9b8e94425c69c0b887e5779ebb8992
Prabhakar Naik Kimavath <prabhakarnaik225@gmail.com>
2024-08-20 19:00:06 +0530

    npm first project added

---
+++ npm first project/index.js
@@ -0,0 +1,19 @@
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+// console.log(`my name is ${sillyName}.`);
+
+// second way
+
+// import generateName from "sillyname";
+
+// const sillyName = generateName();
+
+// console.log(`my name is ${sillyName}.`);
+
+
+import { randomSuperhero } from "superheroes";
+
+const name = randomSuperhero();
+
+console.log(`I am ${name}!.`);
